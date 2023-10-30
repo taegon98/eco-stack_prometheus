97dc87d21a18bfd9374245e269fdc7d3649facb6
taegon1998 <taegon1998@gmail.com>
2023-10-30 20:41:45 +0900

    feat: cpu metrics 수
    libvirt_domain_info_virtual_cpus, libvirt_domain_info_cpu_time_seconds_total

--- src/main/java/openstack/eco_stack/controller/PrometheusDataFetcher.java
+++ src/main/java/openstack/eco_stack/controller/PrometheusDataFetcher.java
@@ -2,6 +2,7 @@ package openstack.eco_stack.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import openstack.eco_stack.domain.Token;
+import openstack.eco_stack.service.CpuService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import java.net.URISyntaxException;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -20,25 +22,19 @@ import java.util.regex.Pattern;
 public class PrometheusDataFetcher {
 
     private final RestTemplate restTemplate;
+    private final CpuService cpuService;
 
     @Autowired
-    public PrometheusDataFetcher(RestTemplateBuilder restTemplateBuilder) {
+    public PrometheusDataFetcher(RestTemplateBuilder restTemplateBuilder, CpuService cpuService) {
         this.restTemplate = restTemplateBuilder.build();
+        this.cpuService = cpuService;
     }
 
     @GetMapping("/prometheus-metrics")
     public ResponseEntity<String> getPrometheusMetrics() throws URISyntaxException {
+        String metricData = fetchMetricsData();
+        String AUTH_TOKEN = new Token(new RestTemplateBuilder()).fetchToken();
 
-        // Prometheus 메트릭 데이터를 가져올 URL 설정
-        String prometheusMetricsURL = "http://133.186.215.103:9000/metrics";
-        Token token = new Token(new RestTemplateBuilder());
-        String AUTH_TOKEN = token.fetchToken();
-
-        // RestTemplate을 사용하여 메트릭 데이터를 문자열로 가져오기
-        ResponseEntity<String> response = restTemplate.getForEntity(prometheusMetricsURL, String.class);
-        String metricData = response.getBody();
-
-        // 정규 표현식 패턴
         Pattern pattern = Pattern.compile("instanceName=\"([^\"]+)\".*projectId=\"([^\"]+)\"");
         Matcher matcher = pattern.matcher(metricData);
 
@@ -48,11 +44,11 @@ public class PrometheusDataFetcher {
             String projectId = matcher.group(2);
 
             HttpHeaders headers = new HttpHeaders();
-            headers.set("X-Auth-Token", AUTH_TOKEN); // AUTH_TOKEN은 토큰 값으로 대체
+            headers.set("X-Auth-Token", AUTH_TOKEN);
 
             HttpEntity<?> entity = new HttpEntity<>(headers);
 
-            String projectURL = "http://133.186.215.103/identity/v3/projects/" + projectId; // projectId는 프로젝트 ID로 대체
+            String projectURL = "http://133.186.215.103/identity/v3/projects/" + projectId;
 
             ResponseEntity<String> project = restTemplate.exchange(projectURL, HttpMethod.GET, entity, String.class);
 
@@ -61,8 +57,26 @@ public class PrometheusDataFetcher {
             result.append("Instance Name: ").append(instanceName).append("\n");
             result.append("Project Id: ").append(projectId).append("\n");
             result.append("Project Info: ").append(projectData).append("\n");
-        }
+            Map<String, Double> cpu_사용시간 = cpuService.cpu_사용시간(metricData);
+            Map<String, Double> cpu_코어수 = cpuService.cpu_코어수(metricData);
 
+            log.info("Number of Cores:");
+            for (Map.Entry<String, Double> mp : cpu_코어수.entrySet()) {
+                log.info("Project ID: " + mp.getKey() + ", CPU Metric: " + mp.getValue());
+            }
+
+            log.info("Usage Time:");
+            for (Map.Entry<String, Double> entry : cpu_사용시간.entrySet()) {
+                log.info("Project ID: " + entry.getKey() + ", Virtual CPU Metric: " + entry.getValue());
+            }
+        }
         return ResponseEntity.ok(result.toString());
     }
+
+    private String fetchMetricsData() {
+        String prometheusMetricsURL = "http://133.186.215.103:9000/metrics";
+        ResponseEntity<String> response = restTemplate.getForEntity(prometheusMetricsURL, String.class);
+        return response.getBody();
+    }
+
 }
